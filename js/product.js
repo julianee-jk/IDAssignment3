888540a7f397e4cbc141fb007f5c4c9e577414e5
kahseng <S10208180@connect.np.edu.sg>
2021-02-03 19:30:24 +0800

    Adjust some css styling in product page

--- js/product.js
+++ js/product.js
@@ -6,11 +6,11 @@ function loadSneakers(sneaker_id) {
     .then(function(data) {
         console.log(data);
         $(".product-img").append(`<img src="${data.media.imageUrl}" />`);
-        $(".product-release-date").html(new Date(data.releaseDate).toDateString());
+        $(".product-release-date").append(new Date(data.releaseDate).toDateString());
         $(".product-header").html(`${data.brand} ${data.year}`);
         $(".product-title").html(data.title);
         $(".product-price").append(data.retailPrice);
-        $(".product-colorway").html(data.colorway);
+        $(".product-colorway").append(data.colorway);
     });
 }
 
