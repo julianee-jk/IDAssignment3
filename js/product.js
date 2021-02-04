b33b5e5214fd65c5aad0fc08fa5b29385c5e140c
julianee-jk <s10202587@connect.np.edu.sg>
2021-02-04 16:58:09 +0800

    Add view productID data

--- js/product.js
+++ js/product.js
@@ -4,17 +4,28 @@ function loadSneakers(sneaker_id) {
     fetch(url)
     .then(response => response.json())
     .then(function(data) {
-        console.log(data);
-        $(".product-img").append(`<img src="${data.media.imageUrl}" />`);
-        $(".product-release-date").append(new Date(data.releaseDate).toDateString());
-        $(".product-header").html(`${data.brand} ${data.year}`);
-        $(".product-title").html(data.title);
-        $(".product-price").append(data.retailPrice);
-        $(".product-colorway").append(data.colorway);
+        $('.product-img').append(`<img src="${data.media.imageUrl}" />`);
+        $('.product-release-date').append(new Date(data.releaseDate).toDateString());
+        $('.product-header').html(`${data.brand} ${data.year}`);
+        $('.product-title').html(data.title);
+        $('.product-price').append(data.retailPrice);
+        $('.product-colorway').append(data.colorway);
     });
 }
 
 $(document).ready(function() {
-    sneaker_id = 'c116b14f-8f00-454b-915c-f3f51c7a297c'
+    sneaker_id = localStorage.getItem('viewProductId')
     loadSneakers(sneaker_id);
+    var shoppingBag = [];
+    var size = "S"
+
+    $(".size > input.btn-check").on("click", function(e){
+        size = e.target.attributes.value.value
+    });
+
+    $("#addToCart").click(function(e) {
+        var qty = $(".qty").val();
+        shoppingBag.push([sneaker_id, qty, size]);
+        localStorage.setItem('shoppingBag', JSON.stringify(shoppingBag))
+    });
 });
