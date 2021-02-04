47aafcab772782460f97e321e4a5b47762c5dcbb
julianee-jk <s10202587@connect.np.edu.sg>
2021-02-04 16:52:21 +0800

    Connect create account form to RestDB

--- js/script.js
+++ js/script.js
@@ -5,3 +5,47 @@ $("#createModalButton").click(function (e) {
 $("#loginModalButton").click(function (e) {
     $('#createModal').modal('hide')
 });
+
+//[STEP 0]: Make sure our document is A-OK
+$(document).ready(function () {
+    //what kind of interface we want at the start 
+    const APIKEY = "601a5d306adfba69db8b6cfc";
+    getAccountData();
+    //[STEP 1]: Create our submit form listener
+    $("#create-button").on("click", function (e) {
+        //prevent default action of the button 
+        e.preventDefault();
+
+        //[STEP 2]: let's retrieve form data
+        let name = $("#create-name").val();
+        let dob = $("#create-dob").val();
+        let password = $("#create-password").val();
+
+        //[STEP 3]: get form values when user clicks on send
+        let jsondata = {
+            "name": name,
+            "dob": dob,
+            "password": password
+        };
+        console.log(jsondata);
+        //[STEP 4]: Create our AJAX settings. Take note of API key
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://sneakerzone-11b9.restdb.io/rest/account-info",
+            "method": "POST",
+            "headers": {
+                "content-type": "application/json",
+                "x-apikey": APIKEY,
+                "cache-control": "no-cache"
+            },
+            "processData": false,
+            "data": JSON.stringify(jsondata)
+        }
+
+        //[STEP 5]: Send our ajax request over to the DB and print response of the RESTDB storage to console.
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+        });
+    });//end click 
+});
