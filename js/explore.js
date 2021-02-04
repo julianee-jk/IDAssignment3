bc1c0ea729c795e4122ceee4bfa4af6155497fab
julianee-jk <s10202587@connect.np.edu.sg>
2021-02-04 16:56:59 +0800

    Add productID to localStorage on click

--- js/explore.js
+++ js/explore.js
@@ -5,19 +5,22 @@ function loadSneakers() {
     fetch(url)
     .then(response => response.json())
     .then(function(data) {
-        console.log(data);
         var sneakers = data;
-        sneakers.map((s) => {
-            $(".sneaker-cards").append(`
-                <div class="sneaker-card">
-                    <img src="${s.media.imageUrl}" />
-                    <span class="sneaker-title">${s.title}</span>
-                    <span class="sneaker-colorway">${s.colorway}</span>
-                    <span class="sneaker-price">$${s.retailPrice}</span>
-                </div>
-            `);
-        });
+        const HTMLstring = sneakers.map((s) => `
+            <li class="sneaker-card" onclick="selectCard('${s.id}')" id="${s.id}">
+                <img src="${s.media.imageUrl}" />
+                <span class="sneaker-title">${s.title}</span>
+                <span class="sneaker-colorway">${s.colorway}</span>
+                <span class="sneaker-price">$${s.retailPrice}</span>
+            </li>
+        `);
+        $(".sneaker-cards").html(HTMLstring);
     });
+}
+
+function selectCard(sneakerId) {
+    localStorage.setItem("viewProductId", sneakerId);
+    window.location.href = "product.html";
 }
 
 $(document).ready(function() {
