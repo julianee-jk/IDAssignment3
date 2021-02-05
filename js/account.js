87368c5022615ddb89b9cb7b4e062e45ddb4386f
julianee-jk <s10202587@connect.np.edu.sg>
2021-02-05 16:09:57 +0800

    Add 'add account balance' modal

--- js/account.js
+++ js/account.js
@@ -4,10 +4,70 @@ $("#logout-button").click(function (e) {
 });
 
 $(document).ready(function () {
+    const APIKEY = "601a5d306adfba69db8b6cfc";
     // Check if user is logged in
     const value = localStorage.getItem('accountLoggedIn')
-
+    getAccountData();
     if (value != null) {
         $('#account-name').html(value);
+    }
+
+    // Get account data from database
+    function getAccountData(limit = 10, all = true) {
+        // Create our AJAX settings
+        let settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://sneakerzone-11b9.restdb.io/rest/account-info",
+            "method": "GET", // Use GET to retrieve info
+            "headers": {
+                "content-type": "application/json",
+                "x-apikey": APIKEY,
+                "cache-control": "no-cache"
+            },
+        }
+
+        // Loop to continously add on data
+        $.ajax(settings).done(function (response) {
+            // On login click, check if login info is same as database info
+            console.log(response);
+            $("#changePass-button").on("click", function (e) {
+                for (let i = 0; i < response.length && i < limit; i++) {
+                    if (value == response[i].name) {
+                        e.preventDefault();
+                        let newPass = $("#change-pass").val();
+                        let id = response[i]._id;
+                        updatePass(id, newPass);
+                        break;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+            });
+        });
+
+        function updatePass(id, newPass) {
+            //@TODO create validation methods for id etc. 
+        
+            var jsondata = { "password": newPass };
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": `https://sneakerzone-11b9.restdb.io/rest/account-info/${id}`,
+                "method": "PUT",
+                "headers": {
+                  "content-type": "application/json",
+                  "x-apikey": APIKEY,
+                  "cache-control": "no-cache"
+                },
+                "processData": false,
+                "data": JSON.stringify(jsondata)
+              }
+              
+              $.ajax(settings).done(function (response) {
+                console.log(response);
+              });
+          }//end updateform function
     }
 });
