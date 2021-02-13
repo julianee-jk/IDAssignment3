a015e163b79168d1a872018c83bac37e79e775b3
julianee-jk <s10202587@connect.np.edu.sg>
2021-02-13 15:57:31 +0800

    Add random feature sneaker & click to product page

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
 $(document).ready(function () {
+    loadFeaturing();
     loadTrending();
     loadLatest();
 });
+
+function loadFeaturing() {
+    var url = `https://example-data.draftbit.com/sneakers?_start=0&_end=420`
+    var i = Math.floor(Math.random() * 421);
+
+    fetch(url)
+    .then(response => response.json())
+    .then(function(data) {
+        var sneaker = data[i]
+        var htmlString = `
+            <div class="carousel-feature-slide" onclick=selectCard('${sneaker.id}')>
+                <img src="${sneaker.media.imageUrl}" class="d-block w-100" alt="..." onclick="selectCard('${sneaker.id}')">
+                <div class="carousel-caption carousel-feature-text d-none d-md-block">
+                    <h5>FEATURING</h5>
+                    <p>${sneaker.title}</p>
+                </div>
+            </div>
+        `
+        $(".featured-sneaker").html(htmlString);
+    });
+}
 
 function loadTrending() {
     var url = "https://example-data.draftbit.com/sneakers?_limit=10"
