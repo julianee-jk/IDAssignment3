d152778035c08274366f379ebeb81629adac734a
julianee-jk <s10202587@connect.np.edu.sg>
2021-02-17 23:17:06 +0800

    Fix account daily streaks

--- js/account.js
+++ js/account.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
         var dailyGCGain = 0;
         var recentlyCollected = false;
         $('#account-name').html(accLoggedIn[1]);
-
         $.ajax({ // Get account data from database
             "async": true,
             "crossDomain": true,
@@ -18,149 +17,123 @@ $(document).ready(function() {
             },
         })
         .done(function(account) {
-                // Check dailystreak and set game coupon
-                if (account.dailyStreak >= 0 && account.dailyStreak <= 3)
-                    dailyGCGain = 1;
-                else if (account.dailyStreak > 3 && account.dailyStreak <= 6)
-                    dailyGCGain = 2;
-                else if (account.dailyStreak > 6 && account.dailyStreak <= 50)
-                    dailyGCGain = 3;
-                    
-                $('#daily-streak-id').html(account.dailyStreak);
-                $('#daily-streak-gc').html(dailyGCGain);
-                $('.account-balance').html(account.balance);
-                $('.account-gc').html(account.coupon);
+            if (account.lastDaily == null) { account.lastDaily = new Date($.now() - 86400000) } // Set new account lastDaily to 1 day ago
+            // Check dailystreak and set game coupon
+            if (account.dailyStreak >= 0 && account.dailyStreak <= 3) dailyGCGain = 1;
+            else if (account.dailyStreak > 3 && account.dailyStreak <= 6) dailyGCGain = 2;
+            else if (account.dailyStreak > 6 && account.dailyStreak <= 50) dailyGCGain = 3;
+                
+            $('#daily-streak-id').html(account.dailyStreak);
+            $('#daily-streak-gc').html(dailyGCGain);
+            $('.account-balance').html(account.balance);
+            $('.account-gc').html(account.coupon);
+        
+            $('#topUp-balance-button').on("click", function() {
+                var topUpBalance = $('input[name=topUpBalance]:checked').val();
+                switch(topUpBalance) {
+                    case '$10':
+                        topUpValue = 10;
+                        account.balance += topUpValue;
+                        break;
+                    case '$50':
+                        topUpValue = 50;
+                        account.balance += topUpValue;
+                        break;
+                    case '$100':
+                        topUpValue = 100;
+                        account.balance += topUpValue;
+                        break;
+                    case '$150':
+                        topUpValue = 150;
+                        account.balance += topUpValue;
+                        break;
+                }
+                addTransactionInfo(account._id, account.balance, topUpValue, topUpValue, new Date($.now()));
+                $('#topup-bal-text').html('');
+                $('#topup-bal-text').append(`Added $${topUpValue} to balance.`);
+                $('#topup-bal-text').show();
+                setInterval(function(){$('#topup-bal-text').hide();}, 3000);
+                updateAccount(account);
+            });
+
+            $('#topUp-gc-button').on("click", function() {
+                var topUpGC = $('input[name=topUpGC]:checked').val();
+                var gcCost = 0, coupon = 0;
+
+                switch(topUpGC) {
+                    case '1GC':
+                        coupon = 1;
+                        gcCost = 3; // 1GC for $3
+                        break;
+                    case '5GC':
+                        coupon = 5;
+                        gcCost = 14; // 5GC for $14
+                        break;
+                    case '15GC':
+                        coupon = 15;
+                        gcCost = 43; // 15GC for $43
+                        break;
+                    case '50GC':
+                        coupon = 50;
+                        gcCost = 145; // 50GC for $145
+                        break;
+                }
+
+                if (account.balance - gcCost < 0) { // Check if sufficient account balance
+                    $('#account-gc-error').show(); 
+                }
+
+                else { 
+                    $('#account-gc-error').hide(); 
+                    account.balance -= gcCost;
+                    account.coupon += coupon;
+                    updateAccount(account);
+                }
+            });
             
-                $('#topUp-balance-button').on("click", function() {
-                    var topUpBalance = $('input[name=topUpBalance]:checked').val();
-                    switch(topUpBalance) {
-                        case '$10':
-                            topUpValue = 10;
-                            account.balance += topUpValue;
-                            break;
-                        case '$50':
-                            topUpValue = 50;
-                            account.balance += topUpValue;
-                            break;
-                        case '$100':
-                            topUpValue = 100;
-                            account.balance += topUpValue;
-                            break;
-                        case '$150':
-                            topUpValue = 150;
-                            account.balance += topUpValue;
-                            break;
-                    }
-                    addTransactionInfo(account._id, account.balance, topUpValue, topUpValue, new Date($.now()));
-                    $('#topup-bal-text').html('');
-                    $('#topup-bal-text').append(`Added $${topUpValue} to balance.`);
-                    $('#topup-bal-text').show();
-                    setInterval(function(){$('#topup-bal-text').hide();}, 3000);
-                    updateAccount(account);
-                });
+            $("#password-change").submit(function(e) {
+                e.preventDefault();
+                account.password = $("#new-password").val();
+                updateAccount(account);
+                $('#password-change-text').show();
+            });
 
-                $('#topUp-gc-button').on("click", function() {
-                    var topUpGC = $('input[name=topUpGC]:checked').val();
-                    var gcCost = 0, coupon = 0;
+            $("#logout-button").on("click", function() {
+                localStorage.removeItem('accLoggedIn');
+                window.location.href = 'index.html';
+            });
 
-                    switch(topUpGC) {
-                        case '1GC':
-                            coupon = 1;
-                            gcCost = 3; // 1GC for $3
-                            break;
-                        case '5GC':
-                            coupon = 5;
-                            gcCost = 14; // 5GC for $14
-                            break;
-                        case '15GC':
-                            coupon = 15;
-                            gcCost = 43; // 15GC for $43
-                            break;
-                        case '50GC':
-                            coupon = 50;
-                            gcCost = 145; // 50GC for $145
-                            break;
-                    }
+            $("#delete-account").on("click", function() {
+                localStorage.removeItem('accLoggedIn');
+                deleteAccount(account);
+            })
 
-                    if (account.balance - gcCost < 0) { // Check if sufficient account balance
-                        $('#account-gc-error').show(); 
-                    }
-
-                    else { 
-                        $('#account-gc-error').hide(); 
-                        account.balance -= gcCost;
-                        account.coupon += coupon;
-                        updateAccount(account);
-                    }
-                });
-                
-                $("#password-change").submit(function(e) {
-                    e.preventDefault();
-                    account.password = $("#new-password").val();
-                    updateAccount(account);
-                    $('#password-change-text').show();
-                });
-
-                $("#logout-button").on("click", function() {
-                    localStorage.removeItem('accLoggedIn');
-                    window.location.href = 'index.html';
-                });
-
-                $("#delete-account").on("click", function() {
-                    localStorage.removeItem('accLoggedIn');
-                    deleteAccount(account);
-                })
-
-                // On page load, automatically enable button according to daily streak
-                var firstButton = $("#day1Button");
-                // for (let i=0; i<=7; i++) {}
-                if (account.dailyStreak == 1)
-                    firstButton = $("#day2Button");
-                else if (account.dailyStreak == 2)
-                    firstButton = $("#day3Button");
-                else if (account.dailyStreak == 3)
-                    firstButton = $("#day4Button");
-                else if (account.dailyStreak == 4)
-                    firstButton = $("#day5Button");
-                else if (account.dailyStreak == 5)
-                    firstButton = $("#day6Button");
-                else if (account.dailyStreak >= 6)
-                    firstButton = $("#day7Button");
-                $(firstButton).attr("disabled", false);
-                
-                // Every daily button click, go next button, disable previous
-                $('.dailyButton').on("click", function(e) {
-                    if (e.target.innerHTML == 'Day 7') { $(firstButton).attr("disabled", false); } 
-                    else {
-                        firstButton  = firstButton.next();
-                        $(this).attr("disabled", true);
-                        $(firstButton).attr("disabled", false);
-                    }
-
-                    var currentTime = new Date();
-                    var timeSinceLastDaily = currentTime - new Date(account.lastDaily);
-
-                    // Check if last daily collection is between next 24hours & before next 48hours
-                    if (timeSinceLastDaily > 86400000 && timeSinceLastDaily < 172800000) {
-                        account.dailyStreak += 1; // Increase daily streak by 1
-                        account.lastDaily = new Date($.now()); // Set last daily to current date
-                        // Set daily alert text & show
-                        $('#daily-collect-text').html('Daily Collected!');
-                        $('#daily-collect-text').show();
-                        setInterval(function(){$('#daily-collect-text').hide();}, 3000);
-                    }
-                    // If more than 2 days, reset streak.
-                    else if (timeSinceLastDaily > 172800000) { account.dailyStreak = 0; }
-                    // Check if user recently collect daily
-                    else {
-                        $('#daily-collect-text').html('You recently collected your daily! Come back tomorrow!');
-                        $('#daily-collect-text').show();
-                        setInterval(function(){$('#daily-collect-text').hide();}, 3000);
-                        recentlyCollected = true;
-                    }
-
-                    // Check daily streak (Increase coupon gain with higher streaks) & Add coupon
-                    $('#daily-streak-id').html(account.dailyStreak);
+            // On page load, automatically enable button according to daily streak
+            var firstButton = $("#day1Button");
+            if (account.dailyStreak == 1)
+                firstButton = $("#day2Button");
+            else if (account.dailyStreak == 2)
+                firstButton = $("#day3Button");
+            else if (account.dailyStreak == 3)
+                firstButton = $("#day4Button");
+            else if (account.dailyStreak == 4)
+                firstButton = $("#day5Button");
+            else if (account.dailyStreak == 5)
+                firstButton = $("#day6Button");
+            else if (account.dailyStreak >= 6)
+                firstButton = $("#day7Button");
+            $(firstButton).attr("disabled", false);
+            
+            // Every daily button click, go next button, disable previous
+            $('.dailyButton').on("click", function(e) {
+                var streakBroken = false;
+                var currentTime = new Date();
+                var timeSinceLastDaily = currentTime - new Date(account.lastDaily);
+                // Check if last daily collection is between next 24hours & before next 48hours
+                if (timeSinceLastDaily > 86400000 && timeSinceLastDaily < 172800000) {
+                    account.dailyStreak += 1; // Increase daily streak by 1
+                    account.lastDaily = new Date($.now()); // Set last daily to current date
+                    $('#daily-collect-text').html('Daily Collected!'); // Set daily alert text & show
                     if (account.dailyStreak >= 0 && account.dailyStreak <= 3 && recentlyCollected == false) {
                         dailyGCGain = 1;
                         account.coupon += 1;
@@ -171,9 +144,42 @@ $(document).ready(function() {
                         dailyGCGain = 3;
                         account.coupon += 3;
                     }
-                    $('#daily-streak-gc').html(dailyGCGain);
-                    updateAccount(account);
-                });
+                }
+                // If more than 2 days, reset streak.
+                else if (timeSinceLastDaily > 172800000) { 
+                    account.dailyStreak = 0; 
+                    account.lastDaily = new Date($.now() - 86400000); // Set last daily to current date
+                    $('#daily-collect-text').html('You missed a day! Your daily streak has been broken! Claim it again!');
+                    streakBroken = true;
+                }
+                // Check if user recently collect daily
+                else {
+                    $('#daily-collect-text').html('You recently collected your daily! Come back tomorrow!');
+                    recentlyCollected = true;
+                }
+                $('#daily-collect-text').show();
+                setInterval(function(){$('#daily-collect-text').hide();}, 3000);
+
+                // Next button 
+                if (e.target.innerHTML == 'Day 7') { $(firstButton).attr("disabled", false); } 
+                else {
+                    if (recentlyCollected == false) {
+                        firstButton = firstButton.next();
+                        $(this).attr("disabled", true);
+                        $(firstButton).attr("disabled", false);
+                        updateAccount(account);
+                    }
+                    if (streakBroken == true) {
+                        $(firstButton).attr("disabled", true);
+                        $('#day1Button').attr("disabled", false);
+                        firstButton = $("#day1Button");
+                    }
+                }
+
+                // Check daily streak (Increase coupon gain with higher streaks) & Add coupon
+                $('#daily-streak-id').html(account.dailyStreak);
+                $('#daily-streak-gc').html(dailyGCGain);
+            });
         })
         .fail(function() { window.location.href = 'index.html' });
     }
@@ -271,7 +277,7 @@ function accountDashboard(id) {
         var winningsArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         
         for (let i = 0; i < transaction.length; i++) {
-            if (transaction[i].purchaseType == 'Product' || transaction[i].purchaseType == 'BalanceTopUp') {
+            if (transaction[i].purchaseType == 'Product') {
                 for (let x = 0; x < 12; x++) {
                     if (new Date(transaction[i].purchaseDateTime).getMonth() == x) spendingArray[x] += transaction[i].moneySpent;
                     else continue;
